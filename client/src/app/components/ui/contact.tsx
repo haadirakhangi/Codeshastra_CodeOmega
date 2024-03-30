08e947d1d8b382bf4c250d4b60d9d82ed8a218d2
Mehek <mehekjain28@gmail.com>
2024-03-30 11:49:06 +0530

    client added

---
+++ client/src/app/components/ui/contact.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Globe, World } from "./globe";
+const globeConfig = {
+    pointSize: 1,
+    globeColor: "#1d072e",
+    showAtmosphere: true,
+    atmosphereColor: "#ffffff",
+    atmosphereAltitude: 0.1,
+    polygonColor: "rgba(255,255,255,0.7)",
+    emissive: "#000000",
+    emissiveIntensity: 0.1,
+    shininess: 0.9,
+    arcTime: 2000,
+    arcLength: 0.9,
+    rings: 1,
+    maxRings: 3,
+    autoRotate: true,
+    autoRotateSpeed: 1,
+};
+const data = [
+    {
+        order: 1,
+        startLat: 37.7749,
+        startLng: -122.4194,
+        endLat: 51.5074,
+        endLng: -0.1278,
+        arcAlt: 0.5,
+        color: "#ff0000",
+    },
+    // Add more positions as needed
+];
+
+export const Contact = () => {
+    const servicesItems = ["Mobile development", "UI/UX Design", "web development", "SEO"]
+    return (
+        <main className="flex overflow-hidden">
+            <div className="flex-1 hidden lg:block">
+                {/* -------------------------- */}
+                <World globeConfig={globeConfig} data={data} />
+            </div>
+            <div className="py-12 flex-1 lg:flex lg:justify-center lg:h-screen lg:overflow-auto">
+                <div className="max-w-lg flex-1 mx-auto px-4 text-gray-600">
+                    <div>
+                        <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                            Get in touch
+                        </h3>
+                        <p className="mt-3">
+                            We’d love to hear from you! Please fill out the form below.
+                        </p>
+                    </div>
+                    <form
+                        onSubmit={(e) => e.preventDefault()}
+                        className="space-y-5 mt-12 lg:pb-12"
+                    >
+
+                        <div>
+                            <label className="font-medium">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                            />
+                        </div>
+                        <div>
+                            <label className="font-medium">
+                                Phone number
+                            </label>
+                            <div className="relative mt-2">
+                                <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2">
+                                    <select className="text-sm bg-transparent outline-none rounded-lg h-full">
+                                        <option>US</option>
+                                        <option>ES</option>
+                                        <option>MR</option>
+                                    </select>
+                                </div>
+                                <input
+                                    type="number"
+                                    placeholder="+1 (555) 000-000"
+                                    required
+                                    className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label className="font-medium">
+                                Message
+                            </label>
+                            <textarea required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"></textarea>
+                        </div>
+                        <button
+                            className="w-full px-4 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    )
+}
