52e3d487e86af6ff2bfdb61ca09bdee4a8696800
Hastansh <hastu4123@gmail.com>
2024-03-30 23:26:41 +0530

    login register initial

--- server/app.py
+++ server/app.py
@@ -1,214 +1,145 @@
-import streamlit as st
-import os
-from utils import *
-from audio_recorder_streamlit import audio_recorder
-from streamlit_float import *
-import os
-import openai
-import ast
-import json
-from openai import OpenAI
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import pveagle
+from pvrecorder import PvRecorder
 import time
-from langchain_core.utils.function_calling import convert_to_openai_tool
-from langchain_core.pydantic_v1 import BaseModel, Field, validator
-from tools import search_tool, weather_tool, calculator_tool, filter_emails_tool
-from agents import Agents
-from tasks import Tasks
-from crewai import Crew
+import os
+# from flask_socketio import SocketIO, emit
+import wave
+from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///speaker_profiles.db'
+db = SQLAlchemy(app)
+access_key=os.environ.get('PICOVOICE_API')
 
-float_init()
+bcrypt = Bcrypt(app)
 
-tools = [
-    {
-        'type': 'function',
-        'function': 
-         {
-              'name': 'search_tool',
-              'description': 'Get information about anything from the internet',
-              'parameters': {
-                  'type': 'object',
-                  'properties': {
-                      'query': {
-                          'description': 'The query to use to search on the internet',
-                          'type': 'string'
-                          }
-                      },
-                    'required': ['query']
-                  }
-              }
-        },
-    {
-        'type': 'function',
-        'function': {
-            'name': 'weather_tool',
-            'description': 'Fetch current temperature for given coordinates.',
-            'parameters': {
-                'type': 'object',
-                'properties': {
-                    'latitude': {
-                        'description': 'Latitude of the location to fetch weather data for',
-                        'type': 'number'
-                        },
-                    'longitude': {
-                        'description': 'Longitude of the location to fetch weather data for',
-                        'type': 'number'
-                        }
-                    },
-                  'required': ['latitude', 'longitude']
-                }
-            }
-        },
-        {
-            'type': 'function',
-            'function': {
-                'name': 'calculator_tool',
-                'description': "Performs a specified mathematical operation like sum, minus, multiplication, division, etc. The input to this tool should be a mathematical expression, a couple examples are `200*7` or `5000/2*10`",
-                'parameters': {
-                    'type': 'object',
-                    'properties': {
-                        'operation': {
-                            'description': 'The mathematical operations to perform',
-                            'type': 'string'
-                            }
-                        },
-                'required': ['operation']
-                }
-            }
-        },
-        {
-            'type': 'function',
-            'function': {
-                'name': 'filter_emails_tool',
-                'description': "Use to filter out non-essential emails like news letters and promotional content from gmail.",
-            }
-        }
-]
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    voice_data = db.Column(db.LargeBinary, nullable=True)
 
-available_tools = {
-    'search_tool': search_tool,
-    'weather_tool': weather_tool,
-    'calculator_tool': calculator_tool,
-    'filter_emails_tool': filter_emails_tool
-}
-
-def wait_on_run(run_id, thread_id):
-        while True:
-            run = CLIENT.beta.threads.runs.retrieve(
-                thread_id=thread_id,
-                run_id=run_id,
-            )
-            print('RUN STATUS', run.status)
-            time.sleep(0.5)
-            if run.status in ['failed', 'completed', 'requires_action']:
-                return run
-
-def submit_tool_outputs(thread_id, run_id, tools_to_call):
-    tools_outputs = []
-    with st.spinner("Taking Action..."):     
-        for tool in tools_to_call:
-            output = None
-            tool_call_id = tool.id
-            tool_name = tool.function.name
-            tool_args = tool.function.arguments
-            print('TOOL CALLED:', tool_name)
-            print('ARGUMENTS:', tool_args)
-            tool_to_use = available_tools.get(tool_name)
-            if tool_name =='search_tool':
-                query = json.loads(tool_args)['query']
-                output = tool_to_use(query)
-            elif tool_name=='weather_tool':
-                latitude = json.loads(tool_args)['latitude']
-                longitude = json.loads(tool_args)['longitude']
-                output = tool_to_use(latitude=latitude, longitude=longitude)
-            elif tool_name=='calculator_tool':
-                operation = json.loads(tool_args)['operation']
-                output = tool_to_use(operation=operation)
-            elif tool_name=='filter_emails_tool':
-                output = tool_to_use()
-            if output:
-                tools_outputs.append(
-                    {'tool_call_id': tool_call_id, 'output': output})
-
-    return CLIENT.beta.threads.runs.submit_tool_outputs(thread_id=thread_id, run_id=run_id, tool_outputs=tools_outputs)
-
-# Initialize session state for managing chat messages
-def initialize_session_state():
-    assistant = CLIENT.beta.assistants.create(
-        name="Aura",
-        instructions="You are a helpful voice assistant. Think carefully about the user's request and assist the user. Make the best use of the tools provided to you. DO NOT use the tools if it is not required and you can answer the user by yourself.",
-        model="gpt-3.5-turbo-1106",
-        tools=tools
-)
-    thread = CLIENT.beta.threads.create()
-
-    if 'assistant_id' not in st.session_state:
-        st.session_state['assistant_id'] = assistant.id
-    if 'thread_id' not in st.session_state:
-        st.session_state['thread_id'] = thread.id
-    if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "assistant", "content": "Hi! How may I assist you today?"}]
-
-initialize_session_state()
-
-st.title("OpenAI Conversational Chatbot 🤖")
-# Create a container for the microphone and audio recording
-footer_container = st.container()
-with footer_container:
-    audio_bytes = audio_recorder()
+    def __repr__(self):
+        return f"User('{self.firstname}', '{self.lastname}', '{self.email}')"
     
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+# Create the database tables based on your models
+# with app.app_context():
+    # db.create_all()
+# class SpeakerProfile(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     profile_data = db.Column(db.LargeBinary, nullable=False)
 
-   
-if audio_bytes:
-    assistant_id = st.session_state['assistant_id']
-    thread_id = st.session_state['thread_id']
-    with st.spinner("Transcribing..."):
-        # Write the audio bytes to a temporary file
-        webm_file_path = "temp_audio.mp3"
-        with open(webm_file_path, "wb") as f:
-            f.write(audio_bytes)
+#     def _repr_(self):
+#         return f"SpeakerProfile('{self.name}', '{self.profile_data}')"
 
-        # Convert the audio to text using the speech_to_text function
-        transcript = speech_to_text(webm_file_path)
+# Route for user registration
 
-        if transcript:
-            st.session_state.messages.append({"role": "user", "content": transcript})
-            with st.chat_message("user"):
-                st.write(transcript)
-            os.remove(webm_file_path)
-            
- 
-if st.session_state.messages[-1]["role"] != "assistant":
-    message = CLIENT.beta.threads.messages.create(
-        thread_id= thread_id,
-        role="user",
-        content= st.session_state.messages[-1]['content'],
-    )
-    run = CLIENT.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id= assistant_id,
-    )
-    run = wait_on_run(run.id, thread_id)
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking🤔..."):
-            if run.status == 'failed':
-                print('RUN ERROR',run.message)
-            elif run.status == 'requires_action':
-                run = submit_tool_outputs(thread_id, run.id, run.required_action.submit_tool_outputs.tool_calls)
-                run = wait_on_run(run.id,thread_id)
-            messages = CLIENT.beta.threads.messages.list(thread_id= thread_id,order="asc")
-            content = None
-            for thread_message in messages.data:
-                content = thread_message.content
-            chatbotReply = content[0].text.value
-            print('CHATBOT REPLY', chatbotReply)
-        with st.spinner("Generating audio response..."):    
-            audio_file = text_to_speech(chatbotReply)
-            autoplay_audio(audio_file)
-        st.write(chatbotReply)
-        st.session_state.messages.append({"role": "assistant", "content": chatbotReply})
-        os.remove(audio_file)
-           
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.form
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = Users(firstname=data['firstname'], lastname=data['lastname'], email=data['email'], password=hashed_password)
+
+    # Save a sample text file to large binary field
+    file_content = b'This is a sample text file content.'
+    new_user.voice_data = file_content
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+# Route for user login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.form
+    user = Users.query.filter_by(email=data['email']).first()
+
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+def split_audio_into_frames(audio_file, frame_duration_ms):
+    frames = []
+    with wave.open(audio_file, 'rb') as wf:
+        frame_size = int(frame_duration_ms * wf.getframerate() / 1000)  # Calculate frame size in samples
+        while True:
+            frame_data = wf.readframes(frame_size)
+            if not frame_data:
+                break
+            frames.append(frame_data)
+    return frames
+
+def save_speaker(name,eagle_profiler):
+    speaker_profile = eagle_profiler.export()
+    a=speaker_profile.to_bytes()
+    print(a)
+    with open(f'{name}.txt', 'wb') as f:
+        f.write(a)
+
+# def read_speaker():
+    
+
+@app.route('/enroll', methods=['GET']) #HASTANSHHHHH; 
+def enroll_speaker():
+    audio_frames = split_audio_into_frames("test.wav", 1000)  # Split audio into 1-second frames
+    eagle_profiler = pveagle.create_profiler(access_key=access_key)
+    enroll_percentage = 0.0
+    while enroll_percentage < 100.0:  # Read audio frame from recorder
+        enroll_percentage, feedback = eagle_profiler.enroll(audio_frames[0])
+        print(f"Enrollment progress: {enroll_percentage}%")
+        print(feedback)
+    speaker_profile = eagle_profiler.export()
+    print(speaker_profile)
+    # Save the speaker profile to the database
+    # new_speaker = SpeakerProfile(name="Hatim", profile_data=speaker_profile)
+    # db.session.add(new_speaker)
+    # db.session.commit()
+    return jsonify({"message": "Speaker enrolled successfully"}), 200
+
+@app.route('/recognize', methods=['POST'])
+def recognize_speaker():
+    try:
+        eagle = pveagle.create_recognizer(access_key=access_key, speaker_profiles=[])
+        recorder_r = PvRecorder(device_index=-1, frame_length=512)
+        recorder_r.start()  # Start recording
+        print("Recording for speaker recognition...")
+        print()
+
+        consecutive_recognitions = 0
+        recognition_threshold = 4  # Number of consecutive recognitions to consider
+
+        while True:
+            audio_frame = recorder_r.read()  # Read audio frame from recorder
+            scores = eagle.process(audio_frame)
+            highest_score = max(scores)
+            print(highest_score)
+            print(f"Highest score: {highest_score}")  # Debugging line
+            if highest_score > 0.5:
+                consecutive_recognitions += 1
+                if consecutive_recognitions >= recognition_threshold:
+                    print("Speaker recognized for 4 seconds straight. Stopping recording.")
+                    # Retrieve recognized speaker profile data from the database
+                    recognized_profile_data = audio_frame  # Placeholder, replace with actual profile data
+                    recognized_speaker = Users.query.filter_by(profile_data=recognized_profile_data).first()
+                    if recognized_speaker:
+                        recognized_speaker_name = recognized_speaker.name
+                        print(f"Recognized speaker: {recognized_speaker_name}")
+                        return jsonify({"message": "Speaker recognized successfully", "name": recognized_speaker_name}), 200
+                    else:
+                        print("Recognized speaker not found in the database.")
+            else:
+                consecutive_recognitions = 0
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        recorder_r.stop()
+        eagle.delete()
+if __name__== "__main__":
+    app.run(debug=True)
---
+++ server/run1.py
@@ -0,0 +1,214 @@
+import streamlit as st
+import os
+from utils import *
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
+import os
+import openai
+import ast
+import json
+from openai import OpenAI
+import time
+from langchain_core.utils.function_calling import convert_to_openai_tool
+from langchain_core.pydantic_v1 import BaseModel, Field, validator
+from tools import search_tool, weather_tool, calculator_tool, filter_emails_tool
+from agents import Agents
+from tasks import Tasks
+from crewai import Crew
+
+
+float_init()
+
+tools = [
+    {
+        'type': 'function',
+        'function': 
+         {
+              'name': 'search_tool',
+              'description': 'Get information about anything from the internet',
+              'parameters': {
+                  'type': 'object',
+                  'properties': {
+                      'query': {
+                          'description': 'The query to use to search on the internet',
+                          'type': 'string'
+                          }
+                      },
+                    'required': ['query']
+                  }
+              }
+        },
+    {
+        'type': 'function',
+        'function': {
+            'name': 'weather_tool',
+            'description': 'Fetch current temperature for given coordinates.',
+            'parameters': {
+                'type': 'object',
+                'properties': {
+                    'latitude': {
+                        'description': 'Latitude of the location to fetch weather data for',
+                        'type': 'number'
+                        },
+                    'longitude': {
+                        'description': 'Longitude of the location to fetch weather data for',
+                        'type': 'number'
+                        }
+                    },
+                  'required': ['latitude', 'longitude']
+                }
+            }
+        },
+        {
+            'type': 'function',
+            'function': {
+                'name': 'calculator_tool',
+                'description': "Performs a specified mathematical operation like sum, minus, multiplication, division, etc. The input to this tool should be a mathematical expression, a couple examples are `200*7` or `5000/2*10`",
+                'parameters': {
+                    'type': 'object',
+                    'properties': {
+                        'operation': {
+                            'description': 'The mathematical operations to perform',
+                            'type': 'string'
+                            }
+                        },
+                'required': ['operation']
+                }
+            }
+        },
+        {
+            'type': 'function',
+            'function': {
+                'name': 'filter_emails_tool',
+                'description': "Use to filter out non-essential emails like news letters and promotional content from gmail.",
+            }
+        }
+]
+
+available_tools = {
+    'search_tool': search_tool,
+    'weather_tool': weather_tool,
+    'calculator_tool': calculator_tool,
+    'filter_emails_tool': filter_emails_tool
+}
+
+def wait_on_run(run_id, thread_id):
+        while True:
+            run = CLIENT.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run_id,
+            )
+            print('RUN STATUS', run.status)
+            time.sleep(0.5)
+            if run.status in ['failed', 'completed', 'requires_action']:
+                return run
+
+def submit_tool_outputs(thread_id, run_id, tools_to_call):
+    tools_outputs = []
+    with st.spinner("Taking Action..."):     
+        for tool in tools_to_call:
+            output = None
+            tool_call_id = tool.id
+            tool_name = tool.function.name
+            tool_args = tool.function.arguments
+            print('TOOL CALLED:', tool_name)
+            print('ARGUMENTS:', tool_args)
+            tool_to_use = available_tools.get(tool_name)
+            if tool_name =='search_tool':
+                query = json.loads(tool_args)['query']
+                output = tool_to_use(query)
+            elif tool_name=='weather_tool':
+                latitude = json.loads(tool_args)['latitude']
+                longitude = json.loads(tool_args)['longitude']
+                output = tool_to_use(latitude=latitude, longitude=longitude)
+            elif tool_name=='calculator_tool':
+                operation = json.loads(tool_args)['operation']
+                output = tool_to_use(operation=operation)
+            elif tool_name=='filter_emails_tool':
+                output = tool_to_use()
+            if output:
+                tools_outputs.append(
+                    {'tool_call_id': tool_call_id, 'output': output})
+
+    return CLIENT.beta.threads.runs.submit_tool_outputs(thread_id=thread_id, run_id=run_id, tool_outputs=tools_outputs)
+
+# Initialize session state for managing chat messages
+def initialize_session_state():
+    assistant = CLIENT.beta.assistants.create(
+        name="Aura",
+        instructions="You are a helpful voice assistant. Think carefully about the user's request and assist the user. Make the best use of the tools provided to you. DO NOT use the tools if it is not required and you can answer the user by yourself.",
+        model="gpt-3.5-turbo-1106",
+        tools=tools
+)
+    thread = CLIENT.beta.threads.create()
+
+    if 'assistant_id' not in st.session_state:
+        st.session_state['assistant_id'] = assistant.id
+    if 'thread_id' not in st.session_state:
+        st.session_state['thread_id'] = thread.id
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "Hi! How may I assist you today?"}]
+
+initialize_session_state()
+
+st.title("OpenAI Conversational Chatbot 🤖")
+# Create a container for the microphone and audio recording
+footer_container = st.container()
+with footer_container:
+    audio_bytes = audio_recorder()
+    
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+   
+if audio_bytes:
+    assistant_id = st.session_state['assistant_id']
+    thread_id = st.session_state['thread_id']
+    with st.spinner("Transcribing..."):
+        # Write the audio bytes to a temporary file
+        webm_file_path = "temp_audio.mp3"
+        with open(webm_file_path, "wb") as f:
+            f.write(audio_bytes)
+
+        # Convert the audio to text using the speech_to_text function
+        transcript = speech_to_text(webm_file_path)
+
+        if transcript:
+            st.session_state.messages.append({"role": "user", "content": transcript})
+            with st.chat_message("user"):
+                st.write(transcript)
+            os.remove(webm_file_path)
+            
+ 
+if st.session_state.messages[-1]["role"] != "assistant":
+    message = CLIENT.beta.threads.messages.create(
+        thread_id= thread_id,
+        role="user",
+        content= st.session_state.messages[-1]['content'],
+    )
+    run = CLIENT.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id= assistant_id,
+    )
+    run = wait_on_run(run.id, thread_id)
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking🤔..."):
+            if run.status == 'failed':
+                print('RUN ERROR',run.message)
+            elif run.status == 'requires_action':
+                run = submit_tool_outputs(thread_id, run.id, run.required_action.submit_tool_outputs.tool_calls)
+                run = wait_on_run(run.id,thread_id)
+            messages = CLIENT.beta.threads.messages.list(thread_id= thread_id,order="asc")
+            content = None
+            for thread_message in messages.data:
+                content = thread_message.content
+            chatbotReply = content[0].text.value
+            print('CHATBOT REPLY', chatbotReply)
+        with st.spinner("Generating audio response..."):    
+            audio_file = text_to_speech(chatbotReply)
+            autoplay_audio(audio_file)
+        st.write(chatbotReply)
+        st.session_state.messages.append({"role": "assistant", "content": chatbotReply})
+        os.remove(audio_file)
+           
