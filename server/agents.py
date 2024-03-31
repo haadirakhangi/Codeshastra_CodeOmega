e575f9a8010b012b81bd4664e98edc6dd5f02642
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2024-03-31 08:19:56 +0530

    final change in script

--- server/agents.py
+++ server/agents.py
@@ -5,12 +5,16 @@ from crewai_tools import (
     DirectoryReadTool,
     FileReadTool,
 )
+
+import os
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_API_KEY_2 = os.environ.get('OPENAI_API_KEY_2')
 class Agents():
         def email_filter_agent():
          return Agent(
             role='Senior Email Analyst',
             goal='Filter out non-essential emails like news letters and promotional content',
-            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106'),
+            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106', openai_api_key = OPENAI_API_KEY),
             backstory=dedent ("""
                 As a Senior Email Analyst, you have extensive experience in email content analysis.
                 You are adept at distinguishing important emails from spam, newsletters, and other irrelevant content. Your expertise lies in identifying key patterns and markers that signify the importance of an email."""),
@@ -22,7 +26,7 @@ class Agents():
              return Agent(
             role='Senior Email Drafter',
             goal="Creates a draft of an email based on the user's request.",
-            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106'),
+            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106',openai_api_key = OPENAI_API_KEY),
             backstory=dedent ("""
                 ***Background***:
                 In this role, you possess substantial expertise in crafting compelling email content. Your proficiency lies in interpreting user requests accurately and skillfully composing emails with clear and nuanced language. You utilize your seasoned experience to comprehend the user's request thoroughly. You employ nuanced language to effectively convey the intended message.
@@ -35,7 +39,7 @@ class Agents():
              return Agent(
             role='Senior Email Writer',
             goal="Creates and sends an email based on the user's request.",
-            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106'),
+            llm= ChatOpenAI(model_name='gpt-3.5-turbo-1106',openai_api_key = OPENAI_API_KEY_2),
             backstory=dedent ("""
                 ***Background***:
                 In this role, you possess substantial expertise in crafting compelling email content. Your proficiency lies in interpreting user requests accurately and skillfully composing emails with clear and nuanced language. You utilize your seasoned experience to comprehend the user's request thoroughly. You employ nuanced language to effectively convey the intended message. You also send the mail to the proper receipent.
@@ -48,7 +52,7 @@ class Agents():
             return Agent(
             role='Senior Directory Manager and Script Execution',
             goal="Efficiently navigate and search user directories, generating tailored OS commands.",
-            llm=ChatOpenAI(model_name='gpt-3.5-turbo-1106'),
+            llm=ChatOpenAI(model_name='gpt-3.5-turbo-1106',openai_api_key = OPENAI_API_KEY_2),
             backstory=dedent("""
                 ***Background***:
                 As a seasoned Senior Directory Manager and Script Execution specialist, you hold extensive expertise in efficiently traversing user directories, discerning their requirements, and crafting precise Windows OS commands to fulfill their needs.
